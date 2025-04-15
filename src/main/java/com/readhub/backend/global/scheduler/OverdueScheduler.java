954e955032761e68ce67609072fb7bcd21066a52
SOOSOO37 <115699805+SOOSOO37@users.noreply.github.com>
2025-04-16 06:31:23 +0900

    feat:연체 자동등록 스케줄러 추가

--- src/main/java/com/readhub/backend/global/scheduler/OverdueScheduler.java
+++ src/main/java/com/readhub/backend/global/scheduler/OverdueScheduler.java
@@ -4,15 +4,15 @@ import com.readhub.backend.overdue.service.OverdueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-//
-//@RequiredArgsConstructor
-//@Component
-//public class OverdueScheduler {
-//
-//    private final OverdueService overdueService;
-//
-//    @Scheduled(cron = "0 0 0 * * *")
-//    public void runAutoRegisterOverdue() {
-//        overdueService.autoRegisterOverdue();
-//    }
-//}
+
+@RequiredArgsConstructor
+@Component
+public class OverdueScheduler {
+
+    private final OverdueService overdueService;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void runAutoRegisterOverdue() {
+        overdueService.autoRegisterOverdue();
+    }
+}
